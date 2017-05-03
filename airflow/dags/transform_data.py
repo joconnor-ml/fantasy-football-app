f2feb1bd6e4ebeefb0d8ad4c0e5a6d45286f9a72
Joseph O'Connor <joseph.m.oconnor.88@gmail.com>
2017-05-03 01:34:54 +0100

    get transform code working

--- airflow/dags/transform_data.py
+++ airflow/dags/transform_data.py
@@ -7,21 +7,31 @@ import numpy as np
 import pandas as pd
 import pymongo
 
-client = pymongo.MongoClient()
-db = client.fantasy_football
-# TODO only grab latest data. currently duplicating.
-player_history = db.player_data.find({},{"history":1})
-player_dfs = {}
-for i, player in enumerate(player_history):
-    if i < 649:
-        continue
-    df = pd.DataFrame(player["history"])
-    df.index = df["round"]
-    player_dfs[i] = df
+def transform_data():
+    client = pymongo.MongoClient()
+    db = client["fantasy_football"]
+    
+    player_history = db["player_data"].find({},{"history":1})
+    player_dfs = {}
+    for i, player in enumerate(player_history):
+        df = pd.DataFrame(player["history"])
+        df = df.reset_index()
+        df.index += df["round"].min()
+        player_dfs[i] = df.select_dtypes(["number"])
+        
+    player_panel = pd.Panel(player_dfs)
+    player_panel.loc[:, :, "appearances"] = player_panel.loc[:, :, "minutes"] > 0
+    player_panel.loc[:, :, "none"] = 1
+        
+    player_details = pd.DataFrame(list(db["elements"].find()))
+    player_details.index = player_details.id
+    
+    player_panel.loc[:, :, "target"] = player_panel.loc[:, :, "total_points"].shift(-1)
+    cumulative_sums = player_panel.cumsum(axis=1)
+    cumulative_means = cumulative_sums.div(cumulative_sums.loc[:, :, "appearances"]+1e-6, axis=1)
 
-player_panel = pd.Panel(player_dfs)
-player_panel.loc[:, :, "appearances"] = player_panel.loc[:, :, "minutes"] > 0
-player_panel.loc[:, :, "none"] = 1
-
-player_details = pd.DataFrame(db.player_details.find_one({},{"elements":1})["elements"])
-player_details.index = player_details.id
+    cumulative_means = cumulative_means.add_suffix("_mean")
+    pd.concat([cumulative_means, player_panel], axis=2).to_pickle("data.pkl")
+    
+if __name__ == "__main__":
+    transform_data()
